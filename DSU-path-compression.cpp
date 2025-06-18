7dacf773a1060c1def5d31726964223d13eb0b79
Afruja Akter Afrin <139883915+Afruja-Afrin@users.noreply.github.com>
2025-06-18 16:55:35 +0600

    path compression using common representative for every node

---
+++ DSU-path-compression.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector< int> par;
+
+    int find_Par( int u){
+        if(u == par[ u])
+            return u;
+        return par[ u] = find_Par( par[ u]);
+    }
+
+    void Union( int u, int v){
+        par[ v] = u;
+    }
+
+    int findCircleNum(vector<vector<int>>& adj) {
+        int n = adj.size( );
+
+        for(int i = 0; i < n; i++)
+            par.push_back( i);
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if( adj[ i][ j]){
+                    int u = find_Par( i);
+                    int v = find_Par( j);
+
+                    if( u != v){
+                        Union( u, v);
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(par[ i] == i)
+                ans++;
+        }
+        return ans;
+    }
+};
